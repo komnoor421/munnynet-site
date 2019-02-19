aaaa18f083ad47b15249ade5b446e926d55811ef
Komron Noorbakhsh <komron.noorbakhsh@welltok.com>
2019-02-18 23:49:25 -0500

    fixed header and cta button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import bg from '../../../public/resources/images/jumbo-bg.jpg';
 import './style.scss';
 
 var jumboBgStyle = {
-  background: `url(${bg}) center 0px`,
+  background: `url(${bg}) center -5px`,
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat'
 };
@@ -20,7 +20,9 @@ class Home extends Component {
             <h1 className="display-4 text-center">MunnyNet Business Loans</h1>
             <h1 className="display-3 text-center">help when Bank Loans Cannot</h1>
           </div>
-          {/* <button type="button" className="applyButton btn btn-primary">Get Started</button> */}
+          <div className='mainButton'>
+            <button type="button" className="applyButton cta btn btn-primary">Get Started</button>
+          </div>
         </div>
       </div>
     );
