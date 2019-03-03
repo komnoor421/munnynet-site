61864cf730e7032bb23fa922afb9521a0a2eb144
Komron Noorbakhsh <komron.noorbakhsh@welltok.com>
2019-03-03 16:13:25 -0500

    serve test

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(port);
 console.log('Server started...');
