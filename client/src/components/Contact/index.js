ab0d46887d892ead675404bcb2eb52976e7047a9
Komron Noorbakhsh <komron.noorbakhsh@welltok.com>
2019-03-11 03:16:29 -0400

    added contact form validation

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import PhoneInput from 'react-phone-number-input/basic-input';
 
 import bg from '../../../public/resources/images/contact.jpg';
 
@@ -21,7 +22,8 @@ class Contact extends Component {
       biztype: '',
       phone: '',
       email: '',
-      message: ''
+      message: '',
+      isValid: false
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -31,9 +33,19 @@ class Contact extends Component {
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
-    })
+    });
   }
 
+  validate() {
+    const { name, bizname, biztype, phone, email, message } = this.state;
+    if(name && bizname && phone && email) {
+      let phoneRegex = /^\+?\d{0,3}\s?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}?$/;
+      let emailRegex = /\S+@\S+\.\S+/;
+      return phoneRegex.test(phone) && emailRegex.test(email);
+    } else {
+      return false;
+    }
+  }
 
   sendEmail(e) {
     e.preventDefault();
@@ -47,11 +59,13 @@ class Contact extends Component {
     }).then(function (res){
       console.log(res);
     }).catch(function (err) {
-      console.log(err);
+      console.log('error: ' + error);
     });
   }
 
   render() {
+    const { name, bizname, biztype, phone, email, message } = this.state;
+    const isValid = this.validate();
     return (
       <div id='contactUsWrapper'>
         <div className='jumbotron text-center' style={contentBgStyle}>
@@ -61,32 +75,31 @@ class Contact extends Component {
         </div>
         <section className='contactFormSection'>
           <div className="container">
+          <div className='formInfo'>
+            <span>* Required Fields</span>
+          </div>
           <form id='contactForm' onSubmit={this.sendEmail}>
             <div className="form-group">
-              <label htmlFor="fullName">Full Name / Business Owner</label>
-              <input type="text" className="form-control" id="fullName" placeholder="Enter Name" name='name' required onChange={this.handleChange} />
+              <input type="text" className="form-control" id="fullName" placeholder="Full Name *" name='name' onChange={this.handleChange} />
             </div>
             <div className="form-group">
-              <label htmlFor="fullName">Business Name</label>
-              <input type="text" className="form-control" id="bizName" placeholder="Enter Name" name='bizname' onChange={this.handleChange} />
+              <input type="text" className="form-control" id="bizName" placeholder="Business Name *" name='bizname' onChange={this.handleChange} />
             </div>
             <div className="form-group">
-              <label htmlFor="fullName">Business Type</label>
-              <input type="text" className="form-control" id="bizType" placeholder="Enter Name" name='biztype' onChange={this.handleChange} />
+              <input type="text" className="form-control" id="bizType" placeholder="Business Type" name='biztype' onChange={this.handleChange} />
             </div>
             <div className="form-group">
-              <label htmlFor="phone">Phone</label>
-              <input type="tel" className="form-control" id="phone" placeholder="Enter Phone Number" name='phone' onChange={this.handleChange}/>
+              <input type="tel" className="form-control" id="phone" placeholder="Phone Number *" name='phone' onChange={this.handleChange}/>
             </div>
             <div className="form-group">
-              <label htmlFor="emailHelp">Email address</label>
-              <input type="email" className="form-control" id="email" placeholder="Enter email" name='email' onChange={this.handleChange}/>
+              <input type="email" className="form-control" id="email" placeholder="Email Address *" name='email' onChange={this.handleChange}/>
             </div>
             <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" id="message" name='message' onChange={this.handleChange}></textarea>
+              <textarea className="form-control" id="message" placeholder='Message' name='message' onChange={this.handleChange}></textarea>
             </div>
-            <button type="submit" className="btn btn-primary">Send</button>
+            <div className='submitButtonWrapper'>
+              <button disabled={!isValid} id='submitFormButton' type="submit" className="applyButton btn btn-primary">Apply Now</button>
+            </div>
           </form>
           </div>
         </section>
