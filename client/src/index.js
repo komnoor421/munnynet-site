de7c0986c0755ba76ef89f12b2a949f18a7dd077
Komron Noorbakhsh <komron.noorbakhsh@welltok.com>
2019-03-22 01:39:55 -0400

    some responsive work

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { App } from './components/App';
 
 import './resources/reset.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './resources/styles.scss';
 
 const app = document.getElementById('app');
