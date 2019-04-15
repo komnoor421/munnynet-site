a9471a181abee2a11628b426fe96b168ab921429
Komron Noorbakhsh <komron.noorbakhsh@welltok.com>
2019-04-15 01:49:52 -0400

    syntax error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Layout } from './Layout
+import { Layout } from './Layout';
 import ReactGA from 'react-ga';
 
 export class App extends React.Component {
