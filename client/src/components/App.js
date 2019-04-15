79e695189e41bec18bdf4d3a586e0078b47a4702
Komron Noorbakhsh <komron.noorbakhsh@welltok.com>
2019-04-15 01:46:21 -0400

    testing GA setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Layout } from './Layout';
+import { Layout } from './Layout
+import ReactGA from 'react-ga';
 
 export class App extends React.Component {
   render() {
+    ReactGA.initialize('UA-136779078-1');
+    ReactGA.pageview(window.location.pathname);
     return (
       <BrowserRouter>
         <Layout />
